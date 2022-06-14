375e8817fff861489d41a4abddb6fab4990458e6
Seb Amos <sebastian.amos8@gmail.com>
2022-06-14 13:40:57 +0100

    Tues/Mon

---
+++ daySix/mondayC.js
@@ -0,0 +1,7 @@
+// function backString(str){console.log (str.split("").reverse().join(""));}
+
+// backString("hello")
+
+function dataChecker(cdnn,rank){if (cdnn == "codenation" && rank == 1){console.log(`${cdnn} is number ${rank}`)}
+else{console.log("Try again")} }
+dataChecker("codenation", 1)
